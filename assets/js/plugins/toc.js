d5f593a9913191ef8bfc320c8ef74c523aaac5d3
Беликов Егор Сергеевич <egor-belikov@yandex.ru>
2023-06-08 18:21:14 +0200

    back in future 2

--- assets/js/plugins/toc.js
+++ assets/js/plugins/toc.js
@@ -3,7 +3,7 @@
   $.fn.toc = function(options) {
     var defaults = {
       noBackToTopLinks: false,
-      title: '<i>Содержание</i>',
+      title: '<i>Jump to...</i>',
       minimumHeaders: 3,
       headers: 'h1, h2, h3, h4, h5, h6',
       listType: 'ol', // values: [ol|ul]
